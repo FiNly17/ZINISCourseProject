714c502c2374e35cb3017c1f0f8cb02934e276ba
Daniil Finskiy <danik_fin2001@mail.ru>
2022-05-08 22:03:15 +0300

    KecCak512

---
+++ KecCak512Sha3/KecCak512Sha3/Program.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KecCak512Sha3
+{
+    public class Program
+    {
+        /*const */
+        static private Byte InstanceNumber = 6;
+        /*const */
+        static private UInt16[] b_array = { 25, 50, 100, 200, 400, 800, 1600 }; //permutation_width
+        /*const */
+        static private Byte matrixSize = 5 * 5;
+        /*const */
+        static private Byte[] w_array = { 1, 2, 4, 8, 16, 32, 64 }; //b / 25; 1600 / 25 for 512
+        /*const */
+        static private Byte[] l_array = { 0, 1, 2, 3, 4, 5, 6 }; //log(static_cast<float>(m_w)) / log(2.0F)
+        /*const */
+        static private Byte[] n_array = { 12, 14, 16, 18, 20, 22, 24 }; //12 + 2 * l -> number_of_permutation
+        /*const */
+        static private Byte[,] r = {
+        {0, 36, 3, 41, 18},
+        {1, 44, 10, 45, 2},
+        {62, 6, 43, 15, 61},
+        {28, 55, 25, 21, 56},
+        {27, 20, 39, 8, 14}
+    };
+        /*const */
+        static private UInt64[] RC = {//24 by w_array[KeccakInstanceCount - 1] (for 1600), for less please use less in w_array
+        0x0000000000000001,
+        0x0000000000008082,
+        0x800000000000808A,
+        0x8000000080008000,
+        0x000000000000808B,
+        0x0000000080000001,
+        0x8000000080008081,
+        0x8000000000008009,
+        0x000000000000008A,
+        0x0000000000000088,
+        0x0000000080008009,
+        0x000000008000000A,
+        0x000000008000808B,
+        0x800000000000008B,
+        0x8000000000008089,
+        0x8000000000008003,
+        0x8000000000008002,
+        0x8000000000000080,
+        0x000000000000800A,
+        0x800000008000000A,
+        0x8000000080008081,
+        0x8000000000008080,
+        0x0000000080000001,
+        0x8000000080008008
+    };
+
+        static private UInt64[,] B = new UInt64[5, 5];
+        static private UInt64[] C = new UInt64[5];
+        static private UInt64[] D = new UInt64[5];
+
+        /*const*/
+        static public UInt16[] rate_array = { 576, 832, 1024, 1088, 1152, 1216, 1280, 1344, 1408 };
+        /*const*/
+        static public UInt16[] capacity_array = { 1024, 768, 576, 512, 448, 384, 320, 256, 192 };
+        public enum SHA3 { SHA512 = 0, SHA384, SHA256 = 3, SHA224 };
+
+        static private UInt64[,] Keccak_f(UInt64[,] A)
+        {
+            for (Byte i = 0; i < n_array[InstanceNumber]; i++)
+            {
+                A = Round(A, RC[i]);
+            }
+            return A;
+        }
+
+        static private UInt64[,] Round(UInt64[,] A, UInt64 RC_i)
+        {
+            Byte i, j;
+
+            //theta step
+            for (i = 0; i < 5; i++)
+                C[i] = A[i, 0] ^ A[i, 1] ^ A[i, 2] ^ A[i, 3] ^ A[i, 4];
+            for (i = 0; i < 5; i++)
+                D[i] = C[(i + 4) % 5] ^ ROT(C[(i + 1) % 5], 1, w_array[InstanceNumber]);
+            for (i = 0; i < 5; i++)
+                for (j = 0; j < 5; j++)
+                    A[i, j] = A[i, j] ^ D[i];
+
+            //rho and pi steps
+            for (i = 0; i < 5; i++)
+                for (j = 0; j < 5; j++)
+                    B[j, (2 * i + 3 * j) % 5] = ROT(A[i, j], r[i, j], w_array[InstanceNumber]);
+
+            //chi step
+            for (i = 0; i < 5; i++)
+                for (j = 0; j < 5; j++)
+                    A[i, j] = B[i, j] ^ ((~B[(i + 1) % 5, j]) & B[(i + 2) % 5, j]);
+
+            //iota step
+            A[0, 0] = A[0, 0] ^ RC_i;
+
+            return A;
+        }
+
+        static private UInt64 ROT(UInt64 x, Byte n, Byte w)
+        {
+            return ((x << (n % w)) | (x >> (w - (n % w))));
+        }
+
+        static private Byte[] Keccak(UInt16 rate, UInt16 capacity, List<Byte> Message)
+        {
+            //Padding
+            Message.Add(0x01);
+
+            UInt16 min = (UInt16)((rate - 8) / 8); 
+            UInt16 n = (UInt16)Math.Ceiling(((float)Message.Count / (float)min));
+            int messageFullCount = 0;
+
+            //Why if n<2?
+            if (n < 2)
+            {
+                messageFullCount = min;
+            }
+            else
+            {
+                messageFullCount = (n * min + (n - 1));
+            }
+
+            UInt32 delta = (UInt32)(messageFullCount - Message.Count);
+
+            //Check size of message.
+            if (messageFullCount > UInt16.MaxValue - 1)
+                throw (new Exception("Message might be too large"));
+
+            while (delta > 0)  // Дополнение нулевыми битами до тех пор, пока длина не будет кратна Rate
+            {
+                Message.Add(0x00);
+                delta--;
+            }
+
+            if ((Message.Count * 8 % rate) != (rate - 8))
+                throw (new Exception("Length was incorect calculated"));
+
+            Message.Add(0x80); //Обозначает конец сообщения
+            /*const*/
+            Int32 size = (Message.Count * 8) / rate;
+
+            UInt64[,] P = new UInt64[size, matrixSize];
+            Int32 xF = 0, count = 0;
+            Byte i = 0, j = 0;
+            int xd=0;
+            int q = 0;
+
+            for (xF = 0; xF < Message.Count; xF++)
+            {
+                if (j > (rate / w_array[InstanceNumber] - 1))
+                {
+                    j = 0;
+                    i = 0;
+                }
+                count++;
+                if ((count * 8 % w_array[InstanceNumber]) == 0)
+                {
+                    P[xd, i + j] = ReverseEightBytesAndToUInt64(
+                        Message.GetRange(count - w_array[InstanceNumber] / 8, 8).ToArray()
+                        );
+                    q++;
+                    if (xF == 71)
+                        xd++;
+                    j++;
+                }
+            }
+
+            //Initialization
+            UInt64[,] S = new UInt64[5, 5];
+            for (i = 0; i < 5; i++)
+                for (j = 0; j < 5; j++)
+                    S[i, j] = 0;
+
+            //Absorting phase
+            for (xF = 0; xF < size; xF++)
+            {
+                for (i = 0; i < 5; i++)
+                    for (j = 0; j < 5; j++)
+                        if ((i + j * 5) < (rate / w_array[InstanceNumber]))
+                        {
+                            S[i, j] = S[i, j] ^ P[xF, i + j * 5];
+                        }
+                S = Keccak_f(S);
+
+                Console.WriteLine($"{xF + 1} step:");
+                foreach (var x in S)
+                {
+                    Console.Write(ByteArrayToString(FromUInt64ToReverseEightBytes(x)).ToString());
+                }
+                Console.WriteLine();
+            }
+
+            //Squeezing phaze
+            Console.WriteLine();
+            Byte a = 0;
+            Byte d_max = (Byte)(capacity / (2 * 8));
+            List<Byte> retHash = new List<Byte>(d_max);
+
+            for (; ; )
+            {
+                for (i = 0; i < 5; i++)
+                    for (j = 0; j < 5; j++)
+                        if ((5 * i + j) < (rate / w_array[InstanceNumber]))
+                        {
+                            if (a >= d_max)
+                                i = j = 5;
+                            else
+                            {
+                                retHash.AddRange(FromUInt64ToReverseEightBytes(S[j, i]));
+                                Console.WriteLine(ByteArrayToString(FromUInt64ToReverseEightBytes(S[j, i]).ToArray()));
+                                a = (Byte)retHash.Count;
+                            }
+                        }
+                if (a >= d_max)
+                    break;
+                S = Keccak_f(S);
+            }
+
+            return retHash.GetRange(0, d_max).ToArray();
+        }
+
+        static private UInt64 ReverseEightBytesAndToUInt64(Byte[] bVal)
+        {
+            UInt64 ulVal = 0L;
+            for (Byte i = 8, j = 0; i > 0; i--)
+            {
+                ulVal += (UInt64)((bVal[i - 1] & 0xF0) >> 4) * (UInt64)Math.Pow(16.0F, 15 - (j++));
+                ulVal += (UInt64)(bVal[i - 1] & 0x0F) * (UInt64)Math.Pow(16.0F, 15 - (j++));
+            }
+            return ulVal;
+        }
+
+        static private byte[] FromUInt64ToReverseEightBytes(UInt64 ulVal)
+        {
+            byte[] bVal = new byte[8];
+            Byte a = 0;
+            do
+            {
+                bVal[a] = (Byte)((ulVal % 16) * 1);
+                ulVal = ulVal / 16;
+                bVal[a] += (Byte)((ulVal % 16) * 16);
+                a++;
+            }
+            while (15 < (ulVal = ulVal / 16));
+            while (a < 8)
+            {
+                bVal[a++] = (Byte)ulVal;
+                ulVal = 0;
+            }
+
+            return bVal;
+        }
+
+        static void Main()
+        {
+            string message;
+            do
+            {
+                Console.Write("Введите сообщение для зашифрования: ");
+                message = Console.ReadLine();
+            } while (message.Length < 1);
+
+            List<Byte> MessageB;
+            //String message = string.Copy(args[0]);
+
+            MessageB = strToByteList(message);
+            String hash_512 = ByteArrayToString(Keccak(rate_array[(Byte)SHA3.SHA512], capacity_array[(Byte)SHA3.SHA512], MessageB));
+
+            Console.WriteLine("Message: " + message + "\r\n" +
+                      "Hash_512: " + hash_512 + "\r\n");
+        }
+
+        static List<byte> strToByteList(string str)
+        {
+            List<byte> byteMessage = new List<byte>(str.Length);
+
+            foreach (char ch in str)
+            {
+                byteMessage.Add((byte)ch);
+            }
+
+            return byteMessage;
+        }
+
+        static public string ByteArrayToString(byte[] b)
+        {
+            StringBuilder sb = new StringBuilder(16);
+            for (int i = 0; i < Math.Min(b.Length, Int32.MaxValue - 1); i++)
+                sb.Append(string.Format("{0:X2}", b[i]));
+            return sb.ToString();
+        }
+    }
+}
